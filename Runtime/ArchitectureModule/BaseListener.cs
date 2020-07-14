84bd02c1023757dae1c9c6b0b307405d4c37fe09
LenweSeregon <serf.nicolas@gmail.com>
2020-07-15 00:26:59 +0200

    [Dev] Base scripts for architecture 1. Base Variable 2. Base Event 3. Base Listener & IListener

---
+++ Runtime/ArchitectureModule/BaseListener.cs
@@ -0,0 +1,35 @@
+﻿namespace Fafnir.Mjollnir.Architecture
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class BaseListener<T, E, UE> : MonoBehaviour, IListener<T> where E : BaseEvent<T> where UE : UnityEvent<T>
+    {
+        [SerializeField] private E _event;
+        [SerializeField] private UE _response;
+
+        private void OnEnable()
+        {
+            if (_event != null)
+            {
+                _event.Register(this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_event != null)
+            {
+                _event.Unregister(this);
+            }
+        }
+
+        public void Raise(T value)
+        {
+            if (_response != null)
+            {
+                _response.Invoke(value);
+            }
+        }
+    }
+}
---
+++ Runtime/ArchitectureModule/IListener.cs
@@ -0,0 +1,7 @@
+﻿namespace Fafnir.Mjollnir.Architecture
+{
+    public interface IListener<T>
+    {
+        void Raise(T value);
+    }
+}
